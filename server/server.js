c6dde3b98ac1026698b1ef5c03f59a9ad2a77d92
YimKeul <leesjun29@gmail.com>
2023-05-31 04:07:21 +0900

    feat : reserveList

--- server/server.js
+++ server/server.js
@@ -89,13 +89,110 @@ app.post("/onLogin", (req, res) => {
       // 쿼리 결과가 참인 경우
       // res.json({ success: "Login successful" });
       res.json(results[0]);
-      console.log(inputId, inputPw);
-      console.log("로그인 성공");
+      // console.log(inputId, inputPw);
+      // console.log("로그인 성공");
     } else {
       // 쿼리 결과가 거짓인 경우
       res.json(results[0]);
-      console.log(inputId, inputPw);
-      console.log("로그인 실패");
+      // console.log(inputId, inputPw);
+      // console.log("로그인 실패");
+    }
+  });
+});
+
+app.get("/reserveList", (req, res) => {
+  const userName = req.query.name;
+
+  const query = `
+    SELECT
+      rc.modelName,
+      rc.licensePlateNo,
+      rs.startDate,
+      rs.endDate
+    FROM
+      Reserve rs
+    JOIN
+      RentCar rc ON rs.licensePlateNo = rc.licensePlateNo
+    JOIN
+      Customer c ON rs.cno = c.cno
+    WHERE
+      c.name = ? AND rs.startDate >= CURDATE();
+  `;
+
+  db.query(query, [userName], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+// 예약
+app.get("/reserve", (req, res) => {
+  const licensePlateNo = req.query.licensePlateNo;
+  const startDate = req.query.startDate;
+  const reserveDate = req.query.reserveDate;
+  const endDate = req.query.endDate;
+  const customerName = req.query.name;
+
+  const query = `
+    INSERT INTO Reserve (licensePlateNo, startDate, reserveDate, endDate, cno)
+    VALUES (
+      ?,
+      ?,
+      ?,
+      ?,
+      (SELECT cno FROM Customer WHERE name = ?)
+    );
+  `;
+
+  db.query(
+    query,
+    [licensePlateNo, startDate, reserveDate, endDate, customerName],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+
+      res.json({ message: "Reservation created successfully" });
+      console.log(result);
+    }
+  );
+});
+
+// 예약 취소
+app.get("/cancelReserve", (req, res) => {
+  const licensePlateNo = req.query.licensePlateNo;
+  const startDate = req.query.startDate;
+  const customerName = req.query.name;
+
+  const query = `
+    DELETE FROM Reserve
+    WHERE licensePlateNo = ? 
+    AND startDate = ?
+    AND cno = (
+      SELECT cno
+      FROM Customer
+      WHERE name = ?
+    );
+  `;
+
+  db.query(query, [licensePlateNo, startDate, customerName], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+
+    if (result.affectedRows === 0) {
+      res.status(404).json({ error: "No matching reservation found" });
+    } else {
+      res.json({ message: "Reservation canceled successfully" });
     }
   });
 });
